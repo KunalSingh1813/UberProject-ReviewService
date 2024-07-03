1a6510726ccc7faaaf75b9844da899da22b2e894
Kunal Singh <singh.kunal1813@gmail.com>
2024-07-03 15:48:01 +0530

    Added Review Repository using JPARepository

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
@@ -12,6 +13,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "BookingReview")
 
@@ -25,14 +27,21 @@ public class Review {
     private String content;
 
     private Double rating;
+
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP) //this annotation tells spring about the format of date object to be stored i.e date, time or timestamp
     @CreatedDate //this annotation tells spring that only handle it for object creation
     private Date createdAt;
+
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate  ///this annotation tells spring that only handle it for object update
     private Date updatedAt;
+
+    @Override
+    public String toString(){
+        return "Review: " + this.content + " " +this.rating + this.createdAt;
+    }
 
 
 }
--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,15 +1,33 @@
 package com.example.UberReviewService.services;
 
 import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class ReviewService implements CommandLineRunner {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
     @Override
     public void run(String... args) throws Exception {
         System.out.println("inside ReviewService----------");
-        //Review r = Review.builder().content("Amazing ride quality").rating(5.0).build(); //code to create plain java object
-        //reviewRepository.save(r);
+        Review r = Review.builder()
+                .content("Good ride quality")
+                .rating(4.0).build(); //code to create plain java object
+        System.out.println(r);
+        reviewRepository.save(r); //implemented by JpaRepository
+
+        List<Review> reviews = reviewRepository.findAll();
+
+        for(Review review: reviews){
+            System.out.println(r.getContent());
+        }
+
     }
 }
